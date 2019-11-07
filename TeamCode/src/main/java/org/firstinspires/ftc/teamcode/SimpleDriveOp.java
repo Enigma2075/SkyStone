e50c3fb89d4120037d5d9d96677d3a8ad03806b1
Matt Dressel <mdressel@gmail.com>
2019-11-06 19:01:31 -0500

    Rename simpledrive op.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDriveOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDriveOp.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.foundationGrabber.FoundationGrabber;
 import org.firstinspires.ftc.teamcode.intake.Intake;
 import org.firstinspires.ftc.teamcode.sensors.SensorArray;
 
-@TeleOp(name="DriveOp", group="Basic")
+@TeleOp(group="Basic")
 public class SimpleDriveOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
